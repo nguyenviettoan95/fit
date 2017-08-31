33e22482c6a1b277a1c43f53daf14076f1908c27
nguyenviettoan95 <30643631+nguyenviettoan95@users.noreply.github.com>
2017-09-01 00:25:02 +0700

    first commit

---
+++ fit.php
@@ -0,0 +1,338 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>FIT</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-them.min.css">
+	<link rel="stylesheet" type="text/css" href="css/Begin_css.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+	<div class="container">
+		<header>
+			<div class="container-fluid">
+				<div class="head">
+					<div class="navbar-head">
+						<h1><img src="img/logohead.png" class="img-responsive" ></h1>
+						<img src="img/hotline.png" class="hotline">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+	                        <span class="sr-only">Toggle navigation</span>
+	                        <span class="icon-bar"></span>
+	                        <span class="icon-bar"></span>
+	                        <span class="icon-bar"></span>
+	                     </button>
+	                     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		                     <ul class="nav navbar-nav">
+		                     	<li><a href="#">GIỚI THIỆU</a></li>
+		                     	<li><a href="#">KIẾN THỨC</a></li>
+		                     	<li><a href="#">HỎI ĐÁP</a></li>
+		                     </ul>
+		                 </div>
+	                 </div>
+				</div>
+			</div>
+		</header>
+		<div class="main">
+			<div class="home">
+			<section>
+				<div class="banner">
+					<img src="img/both.jpg" style="    width: 100%;">
+				</div>
+			</section>
+			<section id="product" style="    display: flex;
+    justify-content: center;
+    background: #691b1d;">
+				<div class="container product">
+					<h3><i class="fa fa-info" aria-hidden="true"></i> THÔNG TIN SẢN PHẨM</h3><hr>
+					<div class="carousel">
+						 <div id="myCarousel" class="carousel slide" data-ride="carousel">
+						 
+						    <!-- Wrapper for slides -->
+						    <div class="carousel-inner">
+						      <div class="item active">
+						        <img src="img/product.jpg"  style="width:100%;">
+						      </div>
+
+						      <div class="item">
+						        <img src="img/product.jpg"  style="width:100%;">
+						      </div>
+						    
+						      <div class="item">
+						        <img src="img/product.jpg" style="width:100%;">
+						      </div>
+						    </div>
+
+						    <!-- Left and right controls -->
+						    <a class="left carousel-control" style="background: transparent;" href="#myCarousel" data-slide="prev">
+						      <span class="glyphicon glyphicon-chevron-left"></span>
+						      <span class="sr-only">Previous</span>
+						    </a>
+						    <a class="right carousel-control" style="background: transparent;"  href="#myCarousel" data-slide="next">
+						      <span class="glyphicon glyphicon-chevron-right"></span>
+						      <span class="sr-only">Next</span>
+						    </a>
+						  </div>
+					</div>
+					<div class="info">
+                                <h5><i class="fa fa-leaf" aria-hidden="true"></i> Thành phần chính</h5>
+                                <p>
+                                    Nấm phục linh, hạt sen, lá sen, trà Polyphenol, thảo quyết minh, Collagen và các phụ liệu khác. Các thành phần làm nên viên nang giảm cân FIT hoàn toàn đến từ thảo mộc tự nhiên, không gây ảnh hưởng xấu đến sức khỏe của người sử dụng.
+                                </p>
+                                <h5><i class="fa fa-check" aria-hidden="true"></i> Công dụng</h5>
+                                <p>
+                                    Chúng tôi cam kết bằng tất cả uy tín và danh dự của mình, bạn sẽ giảm ngay 5Kg chỉ sau 1 tháng sử dụng. Viên nang giảm cân FIT sẽ ngăn ngừa hấp thu chất béo đồng thời đốt cháy lượng mỡ dư thừa trong cơ thể, đặc biệt là lượng mỡ ở các vùng bụng - đùi - bắp tay, đồng thời thành phần Collagen giúp làm đẹp và ngăn ngừa lão hóa da.
+                                </p>
+                                <h5><i class="fa fa-connectdevelop" aria-hidden="true"></i> Cách dùng</h5>
+                                <p>
+                                    Uống duy nhất 1 viên/ngày vào buổi sáng - tốt nhất là 30 phút trước khi ăn sáng.  Khi người dùng đã giảm xuống một mức cân nặng ưng ý, để không bị tăng cân trở lại thì duy trì sử dụng 2 viên/tuần.
+                                </p>
+                                <h5><i class="fa fa-users" aria-hidden="true"></i> Đối tượng sử dụng</h5>
+                                <p>
+                                    FIT SLIM CAPSULES phù hợp cho cả nam & nữ, những người đang trong quá trình ăn kiêng & giảm cân, từ 16 tới 65 tuổi. Phụ nữ có thai, đang cho con bú, người có tiền sử bệnh huyết áp, tim mạch không được sử dụng sản phẩm.
+                                </p>
+                                <h5><i class="fa fa-dropbox" aria-hidden="true"></i> Quy cách đóng gói: <span class="price">30 viên/lọ <i class="fa fa-tag" aria-hidden="true"></i> 650.000 vnđ</span></h5>
+                            </div>
+                            <div class="icon">
+                            	<img src="img/icon1.png">
+                            	<img src="img/icon2.png">
+                            	<img src="img/icon3.png">
+                            	<img src="img/icon4.png">
+                            	
+                            </div>
+				</div>
+			</section>	
+			<section class="info-nsx">
+				<div class="container infonsx">
+                        <h3><i class="fa fa-info" aria-hidden="true"></i> THÔNG TIN NHÀ SẢN XUẤT</h3>
+                        <hr>
+                        <p>
+                            Nhà sản xuất : <b>USA Health Products (HK) Limited</b> - Nơi sản xuất : <b>Hong Kong</b><br/>
+                            Doanh nghiệp nhập khẩu : Cty TNHH Thiết bị y tế FITNESS STORE <br/>
+                            Doanh nghiệp phân phối : Golden Score <br/>
+                            Địa chỉ : Số 17 Tổ 8a Nam Đồng, Đống Đa, Hà Nội - Số điện thoại : 0944 223 224
+                        </p>
+                        <p>
+                            <b>Golden Score, Ltd</b> là đơn vị có thâm niên nhiều năm trong lĩnh vực phân phối nhãn hàng thực phẩm chức năng, đặc biệt là các dòng sản phẩm chức năng hỗ trợ giảm cân. Ngoài <b>FIT</b>, chúng tôi còn là nhà phân phối của nhiều thương hiệu thực phẩm chức nang khác đang được đông đảo người tiêu dùng đón nhận trên thị trường. Phương châm kinh doanh của chúng tôi đó là mang đến những sản phẩm không chỉ <b>TỐT</b> mà còn <b>AN TOÀN</b> với sức khỏe người tiêu dùng.
+                        </p>
+                        <p>
+                            <b>Fit Slim Capsule</b> là một trong số những sản phẩm mà chúng tôi hân hạnh được giới thiệu đến người tiêu dùng Việt Nam. Đây là dòng thực phẩm chức năng giảm cân được chiết xuất hoàn toàn bởi các nguyên liệu thảo dược có nguồn gốc tự nhiên trên dây chuyền hiện đại của Mỹ, được chứng nhận an toàn của <b>FDA</b> và <b>Bộ Y Tế Việt Nam</b>. Khác với những sản phẩm khác trên thị trường, vốn có nguồn gốc trôi nổi, không qua các khâu kiểm định khắt khe, hoặc được sản xuất thủ công dưới dạng "kem trộn hot girl" bằng những công thức không được kiểm chứng lâm sàng, <b>Viên Nang Giảm FIT</b> là một sản phẩm đạt <b>3 YẾU TỐ</b> quan trọng nhất :
+                        </p>
+                        <p>
+                            <b>Hiệu quả tuyệt đối - An toàn tuyệt đối - Có giấy chứng nhận của các cơ quan chức năng</b>
+                        </p>
+                        <p>
+                            Với dòng sản phẩm này, chúng tôi đưa ra 1 cam kết khẳng định uy tín của mình: <b>Hoàn tiền 100%</b> cho khách hàng đã sử dụng liệu trình sản phẩm mà không giảm được cân. Chúng tôi hoàn toàn tin vào sản phẩm của mình, và cam kết này như 1 lời khẳng định rằng chúng tôi sẽ mang niềm tin đó đến cho tất cả các bạn. Với <b>FIT</b>, mọi nỗi lo về cân nặng đều có thể được quên đi nhanh chóng.
+                        </p>
+                        <h4>
+                            Fit Slim Capsule - Viên nang giảm cân an toàn và hiệu quả
+                        </h4>
+                    </div>
+			</section>
+			<section id="felling">
+				<div class="container felling">
+					<h3><i class="fa fa-comments-o" arria-hidden="true"></i>"CẢM NHẬN NGƯỜI SỬ DỤNG"</h3>
+					<hr>
+					<div class="felling-carousel">
+						 <div id="felling-myCarousel" class="carousel slide" data-ride="carousel">
+						 
+						    <!-- Wrapper for slides -->
+						    <div class="carousel-inner">
+						      <div class="item active">
+						        <p style="text-align: center;">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                </p>
+                                <p class="Aname">- Someone famous</p>
+						      </div>
+
+						      <div class="item">
+						        <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                </p>
+                                <p class="Aname">- Someone famous</p>
+						      </div>
+						    
+						      <div class="item">
+						        <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                </p>
+                                <p class="Aname">- Someone famous</p>
+						      </div>
+						    </div>
+
+						    <!-- Left and right controls -->
+						    <a class="left carousel-control" style="background: transparent;" href="#felling-myCarousel" data-slide="prev">
+						      <span class="glyphicon glyphicon-chevron-left"></span>
+						      <span class="sr-only">Previous</span>
+						    </a>
+						    <a class="right carousel-control " style="background: transparent;" href="#felling-myCarousel" data-slide="next">
+						      <span class="glyphicon glyphicon-chevron-right"></span>
+						      <span class="sr-only">Next</span>
+						    </a>
+						  </div>
+					</div>
+				</div>
+			</section>
+			<section id="comment" class="wrap-comment">
+                    <div class="container-fluid" style="    background: #ebebeb;">
+                    <div class="comment-carousel">
+						 <div id="comment-myCarousel" class="carousel slide" data-ride="carousel">
+						 
+						    <!-- Wrapper for slides -->
+						    <div class="carousel-inner">
+						      <div class="item active">
+						       	<div class="item-comment">
+                                <div class="box">
+                                    <div class="question">
+                                        Q:  Sóng của mạng s-wifi yếu/ màn hình thiết bị xuất hiện thông tin “ chờ trong giây lát” ?
+                                    </div>
+                                    <div class="A">
+                                        A:  Tại mỗi AP của S-wifi có bảng thông báo Điểm phát S-wifi, bạn nên di chuyển vị trí đến điểm này để được truy cập S-wifi tốc độ cao
+                                    </div>
+                                    <img class="arrow" src="img/comment-arrow.png" />
+                                </div>
+                                <div class="member">
+                                    <img src="img/avatar.png"/>
+                                    <div class="info-p">
+                                        <p class="name">Phúc Phằng Phặc</p>
+                                        <p class="email">#phucthemen@gmail.com</p>
+                                    </div>
+                                </div>
+                            </div>
+						      </div>
+
+						      <div class="item">
+						       	<div class="item-comment">
+                                <div class="box">
+                                    <div class="question">
+                                        Q:  Đang sử dụng Internet mà bị mất kết nối?
+                                    </div>
+                                    <div  class="A">
+                                        A:  Trường hợp này thường gặp do lỗi kết nối thiết bị, bạn nên ngắt kết nối (disconnect) và kết nối lại mạng S-wifi.
+                                    </div>
+                                    <img class="arrow" src="img/comment-arrow.png" />
+                                </div>
+                                <div class="member">
+                                    <img src="img/avatar2.png"/>
+                                    <div class="info-p">
+                                        <p class="name">Hạ Hóm Hĩnh</p>
+                                        <p class="email">#ha.h0mhinh@gmail.com</p>
+                                    </div>
+                                </div>
+                            </div>
+						      </div>
+						    
+						      <div class="item">
+						        <div class="item-comment">
+                                <div class="box">
+                                    <div class="question">
+                                       Q:  Không vào được S-wifi thì người nông dân này phải nàm thao đây?
+                                    </div>
+                                    <div  class="A">
+                                       A:  Trường hợp này có thể xuất hiện do thiết bị lần đầu tiên kết nối với S-wifi, bạn nên vào lại www.s-wifi.vn để kết nối lại
+                                    </div>
+                                    <img class="arrow" src="img/comment-arrow.png" />
+                                </div>
+                                <div class="member">
+                                    <img src="img/avatar3.png"/>
+                                    <div class="info-p">
+                                        <p class="name">Trâm Trần Trụi</p>
+                                        <p class="email">#tr4m.trantrui9x@gmail.com</p>
+                                    </div>
+                                </div>
+                            </div>
+						      </div>
+						    </div>
+
+						    <!-- Left and right controls -->
+						    <a class="left carousel-control " style="background: transparent;" href="#comment-myCarousel" data-slide="prev">
+						      <span class="glyphicon glyphicon-chevron-left"></span>
+						      <span class="sr-only">Previous</span>
+						    </a>
+						    <a class="right carousel-control" style="background: transparent;" href="#comment-myCarousel" data-slide="next">
+						      <span class="glyphicon glyphicon-chevron-right"></span>
+						      <span class="sr-only">Next</span>
+						    </a>
+						  </div>
+					</div>
+                            
+                            
+                    </div>
+            </section>
+			</div>
+		</div>
+		<footer>
+			<div class="container footer-m">
+				<div class="row">
+					<div class="col-md-4 company">
+						<div class="title">
+							<h4>VỀ CÔNG TY    <i  class="fa fa-info-circle" aria-hidden="true"></i></h4>
+						</div>
+						<p>Sed aliquet dui auctor blandit ipsum tincidunt Quis rhoncus lorem dolor eu sem. Aenean enim risus, convallis id ultrices eget.</p>
+						<p class="contact">
+                            <i class="fa fa-phone" aria-hidden="true"></i> (305) 888-5522<br/>
+                            <i class="fa fa-envelope" aria-hidden="true"></i> spam@sample.com<br/>
+                            <i class="fa fa-link" aria-hidden="true"></i> www.sample.com<br/>
+                            <i class="fa fa-home" aria-hidden="true"></i> 250 Biscayne Blvd. (North) 11st Floor<br/>
+                        </p>
+                        <p class="social">
+                            <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                            <a href="#"><i class="fa fa-skype" aria-hidden="true"></i></a>
+                            <a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+                            <a href="#"><i class="fa fa-wifi" aria-hidden="true"></i></a>
+                            <a href="#"><i class="fa fa-youtube-square" aria-hidden="true"></i></a>
+                        </p>
+					</div>
+					<div class="col-md-4 knowledge">
+						<div class="title">
+							<h4>KIẾN THỨC MỚI <i class="fa fa-newspaper-o" aria-hidden="true"></i></h4>
+						</div>
+						<div class="media">
+						    <div class="media-left">
+						      <img src="img/avatar.png" class="media-object" style="width:84px; height: 84px;">
+						    </div>
+						    <div class="media-body">
+						      <h4 class="media-heading media1">Sed aliquet dui</h4>
+						       <p class="date">23.02.16</p>
+                                <p>Sed pharetra lorem ut dolor dignissim, sit amet pretium tortor mattis.</p>
+						      
+						    </div>
+						  </div>
+						  <hr>
+						<div class="media">
+						    <div class="media-left">
+						      <img src="img/avatar2.png" class="media-object" style="width:84px; height: 84px;">
+						    </div>
+						    <div class="media-body">
+						      <h4 class="media-heading media1">Sed aliquet dui</h4>
+						       <p class="date">23.02.16</p>
+                                <p>Maecenas pretium scelerisque erat. <br/>Donec ut velit varius, sodales velit ac</p>
+						      
+						    </div>
+						  </div>  
+					</div>
+					<div class="col-md-4 contact-form">
+						<div class="title">
+							<h4>LIÊN HỆ CHÚNG TÔI <i class="fa fa-envelope" aria-hidden="true"></i></h4>
+						</div>
+						 <form>
+                            <input class="form-control" type="text" name="name" placeholder="Name*" />
+                            <input class="form-control" type="text" name="phone" placeholder="Phone*" />
+                            <textarea class="form-control" rows="4" cols="50" name="comment" placeholder="Comments*"></textarea>
+                            <button type="submit" class="btn btn-default">Submit<!-- <i class="fa fa-angle-right" aria-hidden="true"></i> --></button>
+                        </form>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+				<div class="copyright">
+                	<div class="container">Copyright © 2016 GOLDEN SCORE - Số chứng nhận sản phẩm (số visa) : 17483/2016/ATTP-XNCB - Số đăng kí quảng cáo : 79/2016/XNQC-ATTP</div>
+            </div>
+			</div>
+		</footer>
+	</div>
+	 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+     <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
